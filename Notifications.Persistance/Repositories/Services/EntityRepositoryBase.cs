a995d51566db7fdec0ab26041d5ea2920935cfc8
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-14 14:17:33 +0500

    add EntityRepositoryBase

---
+++ Notifications.Persistance/Repositories/Services/EntityRepositoryBase.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Notifications.Domain.Common.Entities;
+
+namespace Notifications.Persistance.Repositories.Services;
+
+public abstract class EntityRepositoryBase<TEntity, TContext> where TEntity : class, IEntity where TContext : DbContext
+{
+    private readonly DbContext _dbContext;
+    protected TContext DbContext => (TContext)_dbContext;
+
+    protected EntityRepositoryBase(TContext dbContext) => _dbContext = dbContext;
+
+    protected IQueryable<TEntity> Get(Expression<Func<TEntity, bool>>? predicate = default, bool asNoTracking = false)
+    {
+        var initialQuery = DbContext.Set<TEntity>().Where(entity => true);
+
+        if (predicate is not null)
+            initialQuery = initialQuery.Where(predicate);
+
+        if (asNoTracking)
+            initialQuery = initialQuery.AsNoTracking();
+
+        return initialQuery;
+    }
+
+    protected async ValueTask<TEntity?> GetByIdAsync(Guid id, bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+    {
+        var initialQuery = DbContext.Set<TEntity>().Where(entity => true);
+
+        if (asNoTracking)
+            initialQuery = initialQuery.AsNoTracking();
+
+        return await initialQuery.SingleOrDefaultAsync(entity => entity.Id == id, cancellationToken: cancellationToken);
+    }
+
+    protected async ValueTask<IList<TEntity>> GetByIdsAsync(IEnumerable<Guid> ids, bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+    {
+        var initialQuery = DbContext.Set<TEntity>().Where(entity => true);
+
+        if (asNoTracking)
+            initialQuery = initialQuery.AsNoTracking();
+
+        initialQuery = initialQuery.Where(entity => ids.Contains(entity.Id));
+
+        return await initialQuery.ToListAsync();
+    }
+
+    protected async ValueTask<TEntity> CreateAsync(TEntity entity, bool saveChanges = true,
+        CancellationToken cancellationToken = default)
+    {
+        await DbContext.Set<TEntity>().AddAsync(entity, cancellationToken);
+
+        if (saveChanges) await DbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    protected async ValueTask<TEntity> UpdateAsync(TEntity entity, bool saveChanges = true,
+        CancellationToken cancellationToken = default)
+    {
+        DbContext.Set<TEntity>().Update(entity);
+
+        if (saveChanges)
+            await DbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    protected async ValueTask<TEntity> DeleteAsync(TEntity entity, bool saveChanges = true,
+        CancellationToken cancellationToken = default)
+    {
+        DbContext.Set<TEntity>().Remove(entity);
+
+        if (saveChanges)
+            await DbContext.SaveChangesAsync(cancellationToken);
+
+        return entity;
+    }
+
+    protected async ValueTask<TEntity> DeleteByIdAsync(Guid id, bool saveChanges = true,
+        CancellationToken cancellation = default)
+    {
+        var entity = await DbContext.Set<TEntity>()
+                         .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken: cancellation) ??
+                     throw new InvalidOperationException();
+        
+        DbContext.Set<TEntity>().Remove(entity);
+
+        if (saveChanges)
+            await DbContext.SaveChangesAsync(cancellationToken: cancellation);
+
+        return entity;
+    }
+}
