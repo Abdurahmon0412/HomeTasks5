ce900adcc18a25fa2cb3c9c704b384463fde88ef
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-16 11:31:24 +0500

    add Identity Services

---
+++ Notifications.Infrastructure/Common/Identity/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notifications.Application.Commoon.Identity;
+using Notifications.Domain.Entities;
+using Notifications.Domain.Enums;
+using Notifications.Persistance.Repositories.Interfaces;
+
+namespace Notifications.Infrastructure.Common.Identity.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository) => _userRepository = userRepository;
+
+    public ValueTask<IList<User>> GetByIdsAsync(IEnumerable<Guid> userIds, bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+        => _userRepository.GetByIdsAsync(userIds, asNoTracking, cancellationToken);
+
+    public async ValueTask<User?> GetSystemUserAsync(bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        return await _userRepository.Get(user => user.Role == RoleType.System, asNoTracking)
+            .Include(user => user.UserSettings)
+            .SingleOrDefaultAsync(cancellationToken);
+
+    }
+
+    public ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false,
+        CancellationToken cancellation = default)
+        => _userRepository.GetByIdAsync(userId, asNoTracking, cancellation);
+}
---
+++ Notifications.Infrastructure/Common/Identity/Services/UserSettingsService.cs
@@ -0,0 +1,16 @@
+﻿using Notifications.Application.Commoon.Identity;
+using Notifications.Domain.Entities;
+using Notifications.Persistance.Repositories.Interfaces;
+
+namespace Notifications.Infrastructure.Common.Identity.Services;
+
+public class UserSettingsService : IUserSettingsService
+{
+    private readonly IUserSettingsRepository _settingsRepository;
+
+    public UserSettingsService(IUserSettingsRepository settingsRepository) => _settingsRepository = settingsRepository;
+
+    public ValueTask<UserSettings?> GetByIdAsync(Guid userSettingsId, bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+        => _settingsRepository.GetByIdAsync(userSettingsId, asNoTracking, cancellationToken);
+}
