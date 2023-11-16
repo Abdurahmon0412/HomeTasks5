b852855953c1c3d02066e5c9f6876e3bbf529eef
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-16 16:57:17 +0500

    end infrastructure layer

--- Notifications.Infrastructure/Common/Notifications/Services/AggregatorServices/NotificationAggregatorService.cs
+++ Notifications.Infrastructure/Common/Notifications/Services/AggregatorServices/NotificationAggregatorService.cs
@@ -1,20 +1,100 @@
-﻿using Notifications.Application.Commoon.Models.Querying;
+﻿using AutoMapper;
+using Microsoft.Extensions.Options;
+using Notifications.Application.Commoon.Identity;
+using Notifications.Application.Commoon.Models.Querying;
 using Notifications.Application.Commoon.Notifications.Models;
 using Notifications.Application.Commoon.Notifications.Services;
 using Notifications.Domain.Common.Extensions;
 using Notifications.Domain.Entities;
+using Notifications.Domain.Enums;
+using Notifications.Domain.Extensions;
+using Notifications.Infrastructure.Common.Settings;
 
 namespace Notifications.Infrastructure.Common.Notifications.Services.AggregatorServices;
 
 public class NotificationAggregatorService : INotificationAggregatorService
 {
-    public ValueTask<IList<NotificationTemplate>> GetTEmplatesByFilterAsync(NotificationTemplateFilter filter, CancellationToken cancellationToken = default)
+    private readonly IMapper _mapper;
+    private readonly IOptions<NotificationSettigs> _notificationSettings;
+    private readonly ISmsTemplateService _smsTemplateService;
+    private readonly IEmailTemplateService _emailTemplateService;
+    private readonly ISmsOrchestrationService _smsOrchestrationService;
+    private IEmailOrchestrationService _emailOrchestrationService;
+    private IUserSettingsService _userSettingsService;
+    private IUserService _userService;
+
+    public NotificationAggregatorService(
+        IMapper mapper,
+        IOptions<NotificationSettigs> notificationSettings,
+        ISmsTemplateService smsTemplateService,
+        IEmailTemplateService emailTemplateService,
+        ISmsOrchestrationService smsOrchestrationService,
+        IEmailOrchestrationService emailOrchestrationService,
+        IUserSettingsService userSettingsService,
+        IUserService userService)
     {
-        throw new NotImplementedException();
+        _mapper = mapper;
+        _notificationSettings = notificationSettings;
+        _smsTemplateService = smsTemplateService;
+        _emailTemplateService = emailTemplateService;
+        _smsOrchestrationService = smsOrchestrationService;
+        _emailOrchestrationService = emailOrchestrationService;
+        _userSettingsService = userSettingsService;
+        _userService = userService;
     }
 
-    public ValueTask<FuncResult<bool>> SendAsync(NotificationRequest notificationRequest, CancellationToken cancellationToken = default)
+    public async ValueTask<IList<NotificationTemplate>> GetTEmplatesByFilterAsync(NotificationTemplateFilter filter,
+        CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var templates = new List<NotificationTemplate>();
+        if(filter.TemplateType.Contains(NotificationType.Sms))
+            templates.AddRange(await  _smsTemplateService.GetByFilterAsync(filter, cancellationToken:cancellationToken));
+        
+        if(filter.TemplateType.Contains(NotificationType.Email))
+            templates.AddRange(await _emailTemplateService.GetByFilterAsync(filter,cancellationToken:cancellationToken));
+
+        return templates;
+    }
+
+    public async ValueTask<FuncResult<bool>> SendAsync(NotificationRequest notificationRequest,
+        CancellationToken cancellationToken = default)
+    {
+        var sendNotificationTask = async () =>
+        {
+            // If sender is not specified, get system user
+            var senderUser = notificationRequest.SenderUserId.HasValue
+                ? await _userService.GetByIdAsync(notificationRequest.SenderUserId.Value,
+                    cancellationToken: cancellationToken)
+                : await _userService.GetSystemUserAsync(true, cancellationToken: cancellationToken);
+
+            notificationRequest.SenderUserId = senderUser!.Id;
+
+            var receiverUser = _userService.GetByIdAsync(notificationRequest.ReceiverUserId,
+                cancellationToken: cancellationToken);
+            
+            // If notification provider type is not specified, get from receiver user settings
+            // if (!notificationRequest.Type.HasValue && receiverUser!.UserSettings.PreferredNotificationType.HasValue)
+            //     notificationRequest.Type = receiverUser!.UserSettings.PreferredNotificationType!.Value;
+
+            // If user not specified preferred notification type get from settings
+            if (!notificationRequest.Type.HasValue)
+                notificationRequest.Type = _notificationSettings.Value.DefaultNotificationType;
+            
+            var sendNotificationTask = notificationRequest.Type switch
+            {
+                NotificationType.Sms => _smsOrchestrationService.SendAsync(
+                    _mapper.Map<SmsNotificationRequest>(notificationRequest),
+                    cancellationToken),
+                NotificationType.Email => _emailOrchestrationService.SendAsync(
+                    _mapper.Map<EmailNotificationRequest>(notificationRequest),
+                    cancellationToken),
+                _ => throw new NotImplementedException("This type of notification is not supported yet.")
+            };
+
+            var sendNotificationResult = await sendNotificationTask;
+            return sendNotificationResult.Data;
+        };
+
+        return await sendNotificationTask.GetValueAsync();
     }
 }
---
+++ Notifications.Infrastructure/Common/Validatiors/NotificationRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Notifications.Application.Commoon.Identity;
+using Notifications.Application.Commoon.Notifications.Models;
+using Notifications.Domain.Enums;
+
+namespace Notifications.Infrastructure.Common.Validatiors;
+
+public class NotificationRequestValidator : AbstractValidator<NotificationRequest>
+{
+    public NotificationRequestValidator(IUserService userService)
+    {
+        var templatesRequireSender = new List<NotificationTemplateType>
+        {
+            NotificationTemplateType.ReferralNotification
+        };
+
+        RuleFor(request => request.SenderUserId)
+            .NotEqual(Guid.Empty)
+            .NotNull()
+            .When(request => templatesRequireSender.Contains(request.TemplateType))
+            .CustomAsync(async (senderUserId, context, cancellationToken) =>
+            {
+                var user = await userService.GetByIdAsync(senderUserId!.Value, true, cancellationToken);
+
+                if (user is null)
+                    context.AddFailure("Sender user not found");
+            });
+
+        RuleFor(request => request.ReceiverUserId)
+            .NotEqual(Guid.Empty)
+            .CustomAsync(async (receiverUserId, context, cancellationToken) =>
+            {
+                var user = await userService.GetByIdAsync(receiverUserId, true, cancellationToken);
+                if (user is null)
+                    context.AddFailure("Sender user not found");
+            });
+    }
+}
