0980bab81b580ab5ad010364e1295fcb756d6cad
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-23 15:53:18 +0500

    add Verification Entity

---
+++ Interceptor.Domain/Entities/UserInfoVerificationCode.cs
@@ -0,0 +1,6 @@
+﻿namespace Interceptor.Domain.Entities;
+
+public class UserInfoVerificationCode : VerificationCode
+{ 
+    public Guid UserId { get; set; }
+}
---
+++ Interceptor.Domain/Entities/VerificationCode.cs
@@ -0,0 +1,19 @@
+﻿using Interceptor.Domain.Common;
+using Interceptor.Domain.Enums;
+
+namespace Interceptor.Domain.Entities;
+
+public class VerificationCode : Entity
+{
+    public VerificationCodeType CodeType { get; set; }
+
+    public VerificationType Type { get; set; }
+
+    public DateTimeOffset ExpiryTime { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string Code { get; set; } = default!;
+
+    public string VerificationLink { get; set; } = default!;
+}
