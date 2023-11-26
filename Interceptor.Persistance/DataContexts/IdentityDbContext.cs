ea4d6ce261acc819abed331c5c10f1a685415627
Abdurahmon <abdurahmonsadriddinov0412@gmail.com>
2023-11-26 17:04:40 +0500

    add same interceptors

--- Interceptor.Persistance/DataContexts/IdentityDbContext.cs
+++ Interceptor.Persistance/DataContexts/IdentityDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace Interceptor.Persistance.DataContexts;
+﻿using Interceptor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class IdentityDbContext
+namespace Interceptor.Persistance.DataContexts;
+
+public class IdentityDbContext(DbContextOptions<IdentityDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Role> Roles => Set<Role>();
+
+    public DbSet<UserInfoVerificationCode> UserInfoVerificationCodes => Set<UserInfoVerificationCode>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+    }
 }
